a030d828a97ed22da6227fc8c14b89c10e2fc5ac
petrundiy2 <petrundiy1@gmail.com>
2016-02-18 00:27:56 +0300

    Update pacman.py
    added walls, global MAP, class Point(unfixed)

--- pacman.py
+++ pacman.py
@@ -8,15 +8,15 @@ import random
 def init_window():
     pygame.init()
     pygame.display.set_mode((512, 512))
-    pygame.display.set_caption('Packman')
+    pygame.display.set_caption('Pacman')
 
 
-def draw_background(scr, img=None):
+def draw_backgfloor(scr, img=None):
     if img:
         scr.blit(img, (0, 0))
     else:
         bg = pygame.Surface(scr.get_size())
-        bg.fill((0, 0, 0))
+        bg.fill((128, 128, 128))
         scr.blit(bg, (0, 0))
 
 
@@ -43,106 +43,168 @@ class GameObject(pygame.sprite.Sprite):
     def draw(self, scr):
         scr.blit(self.image, (self.screen_rect.x, self.screen_rect.y))
 
-class Wall(GameObject):
-    def __init__(self, x,y,tile_size,map_size):
-        GameObject.__init__(self,'./resources/wall.png',x,y,tile_size,map_size)
-
 
 class Ghost(GameObject):
+    ghosts = []
+    num = 2
     def __init__(self, x, y, tile_size, map_size):
-        GameObject.__init__(self, './resources/ghost.png', x, y, tile_size, map_size)
+        GameObject.__init__(self, 'C:/Users/Petr/PycharmProjects/pacman/resources/ghost.png', x, y, tile_size, map_size)
         self.direction = 0
         self.velocity = 4.0 / 10.0
 
     def game_tick(self):
         super(Ghost, self).game_tick()
+
         if self.tick % 20 == 0 or self.direction == 0:
             self.direction = random.randint(1, 4)
 
         if self.direction == 1:
-            self.x += self.velocity
+            if not is_wall(floor(self.x + self.velocity), self.y):
+                self.x += self.velocity
             if self.x >= self.map_size-1:
                 self.x = self.map_size-1
                 self.direction = random.randint(1, 4)
         elif self.direction == 2:
-            self.y += self.velocity
+            if not is_wall(self.x, floor(self.y + self.velocity)):
+                self.y += self.velocity
             if self.y >= self.map_size-1:
                 self.y = self.map_size-1
                 self.direction = random.randint(1, 4)
         elif self.direction == 3:
-            self.x -= self.velocity
+            if not is_wall(floor(self.x - self.velocity), self.y):
+                self.x -= self.velocity
             if self.x <= 0:
-                self.x = 0
-                self.direction = random.randint(1, 4)
+                    self.x = 0
+                    self.direction = random.randint(1, 4)
         elif self.direction == 4:
-            self.y -= self.velocity
+            if not is_wall(self.x, floor(self.y - self.velocity)):
+                self.y -= self.velocity
             if self.y <= 0:
                 self.y = 0
                 self.direction = random.randint(1, 4)
         self.set_coord(self.x, self.y)
 
+def draw_ghosts(screen):
+    for g in Ghost.ghosts:
+        g.draw(screen)
+def create_ghosts(ts, ms):
+    Ghost.ghosts = [Ghost(5, 5, ts, ms) for i in range(Ghost.num)]
+def tick_ghosts():
+    for g in Ghost.ghosts:
+        g.game_tick()
+class Point(GameObject):
+    def __init__(self, x, y, tile_size, map_size):
+        GameObject.__init__(self, 'C:/Users/Petr/PycharmProjects/pacman/resources/point.png', x, y, tile_size, map_size)
+    def game_tick(self):
+        super(Point, self).game_tick()
 
 class Pacman(GameObject):
     def __init__(self, x, y, tile_size, map_size):
-        GameObject.__init__(self, './resources/pacman.png', x, y, tile_size, map_size)
+        GameObject.__init__(self, 'C:/Users/Petr/PycharmProjects/pacman/resources/pacman.png', x, y, tile_size, map_size)
         self.direction = 0
         self.velocity = 4.0 / 10.0
 
     def game_tick(self):
         super(Pacman, self).game_tick()
         if self.direction == 1:
-            if self.y!=wall.y:
+            if not is_wall(floor(self.x + self.velocity), self.y):
                 self.x += self.velocity
             if self.x >= self.map_size-1:
                 self.x = self.map_size-1
         elif self.direction == 2:
-            self.y += self.velocity
+            if not is_wall(self.x, floor(self.y + self.velocity)):
+                self.y += self.velocity
             if self.y >= self.map_size-1:
                 self.y = self.map_size-1
         elif self.direction == 3:
-            self.x -= self.velocity
+            if not is_wall(floor(self.x - self.velocity), self.y):
+                self.x -= self.velocity
             if self.x <= 0:
                 self.x = 0
         elif self.direction == 4:
-            self.y -= self.velocity
+            if not is_wall(self.x, floor(self.y - self.velocity)):
+                self.y -= self.velocity
             if self.y <= 0:
                 self.y = 0
+            if is_point(self.x, self.y):
+                self.map[-1].append(None)
         self.set_coord(self.x, self.y)
 
-def process_events(events, pacman):
+class Wall(GameObject):
+    def __init__(self, x, y, tile_size, map_size):
+        GameObject.__init__(self, 'C:/Users/Petr/PycharmProjects/pacman/resources/wall.png', x, y, tile_size, map_size)
+    def game_tick(self):
+        super(Wall, self).game_tick()
+
+def is_wall(x, y):
+    return isinstance(MAP.map[int(y)][int(x)], Wall)
+def create_walls(coords, ts, ms):
+    Wall.walls = [Wall(2, 4, ts, ms)]
+
+def is_point(x,y):
+    return isinstance(MAP.map[int(y)][int(x)], Point)
+def create_points(coords, ts, ms):
+    Point.points = [Point(2, 4, ts, ms)]
+
+class Map:
+    def __init__(self, filename, tile_size, map_size):
+        self.map = []
+        f=open(filename, 'r')
+        txt = f.readlines()
+        f.close()
+        for y in range(len(txt)):
+            self.map.append([])
+            for x in range(len(txt[y])):
+                if txt[y][x] == "#":
+                    self.map[-1].append(Wall(x, y, tile_size, map_size))
+                elif txt[y][x]=='.':
+                    self.map[-1].append(Point(x,y,tile_size, map_size))
+                else:
+                    self.map[-1].append(None)
+        self.tile_size = tile_size
+        self.map_size = map_size
+    def draw(self,screen):
+        for y in range(len(self.map)):
+            for x in range(len(self.map[y])):
+                if self.map[y][x]:
+                    self.map[y][x].draw(screen)
+
+
+def process_events(events, packman):
     for event in events:
         if (event.type == QUIT) or (event.type == KEYDOWN and event.key == K_ESCAPE):
             sys.exit(0)
         elif event.type == KEYDOWN:
             if event.key == K_LEFT:
-                pacman.direction = 3
+                packman.direction = 3
             elif event.key == K_RIGHT:
-                pacman.direction = 1
+                packman.direction = 1
             elif event.key == K_UP:
-                pacman.direction = 4
+                packman.direction = 4
             elif event.key == K_DOWN:
-                pacman.direction = 2
+                packman.direction = 2
             elif event.key == K_SPACE:
-                pacman.direction = 0
-
+                packman.direction = 0
 
 if __name__ == '__main__':
     init_window()
     tile_size = 32
     map_size = 16
-    ghost = Ghost(0, 0, tile_size, map_size)
+    create_ghosts(tile_size, map_size)
     pacman = Pacman(5, 5, tile_size, map_size)
-    wall=Wall(8,9,tile_size, map_size)
-    background = pygame.image.load("./resources/background.png")
+#    create_walls(tile_size, map_size)
+    global MAP
+    MAP = Map('C:/Users/Petr/PycharmProjects/pacman/map.txt', tile_size, map_size)
+    backgfloor = pygame.image.load("C:/Users/Petr/PycharmProjects/pacman/resources/background.png")
     screen = pygame.display.get_surface()
 
     while 1:
         process_events(pygame.event.get(), pacman)
         pygame.time.delay(100)
-        ghost.game_tick()
+        tick_ghosts()
         pacman.game_tick()
-        draw_background(screen, background)
+        draw_backgfloor(screen, backgfloor)
         pacman.draw(screen)
-        ghost.draw(screen)
-        wall.draw(screen)
+        draw_ghosts(screen)
+        MAP.draw(screen)
         pygame.display.update()
